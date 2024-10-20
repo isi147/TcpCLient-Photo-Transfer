e6708ad6a542848c51c9a1a29b00a1f508088495
Seyidmammadli_i <Seyidmammadli_i@STHQ0116-01>
2024-10-20 12:26:00 +0400

    Add project files.

---
+++ ConsoleApp49/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+
+var ip = IPAddress.Parse("127.0.0.1");
+var port = 27001;
+
+var listener = new TcpListener(ip, port);
+
+listener.Start(10);
+
+Console.WriteLine($"{listener.Server.LocalEndPoint} listening...");
+var client = listener.AcceptTcpClient();
+Console.WriteLine($"{client.Client.RemoteEndPoint} connected...");
+
+var clientStream = client.GetStream();
+
+
+
+var filePath = @$"C:\Users\{Environment.UserName}\Desktop\nerbala.jpg";
+
+// Way 1
+var bytes = File.ReadAllBytes(filePath);
+clientStream.Write(bytes, 0, bytes.Length);
+
+
+// Way 2
+// FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+// fs.CopyTo(clientStream);
+// fs.Flush();
+
+Console.WriteLine("Completed");
+Console.ReadLine();
